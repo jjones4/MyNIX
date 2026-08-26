8d14e272cef90b977e53c839f830c80bc78945c1
elma <elma@fake.email.com>
2026-08-26 17:33:45 -0500

    Fix tabs in vim

--- MyNIX_1.0/MyNIX_1.0_C89_i8088/h/const.h
+++ MyNIX_1.0/MyNIX_1.0_C89_i8088/h/const.h
@@ -22,15 +22,15 @@
 
 #define EXTERN		extern		/* used in *.h files */
 #define PRIVATE		static		/* PRIVATE x limits the scope of x */
-#define PUBLIC				/* PUBLIC is the opposite of PRIVATE */
-#define FORWARD				/* some compilers require this to be 'static' */
+#define PUBLIC  				/* PUBLIC is the opposite of PRIVATE */
+#define FORWARD	    			/* some compilers require this to be 'static' */
 
-#define TRUE		1		/* used for turning integers into Booleans */
-#define FALSE		0		/* used for turning integers into Booleans */
+#define TRUE		1   		/* used for turning integers into Booleans */
+#define FALSE		0	    	/* used for turning integers into Booleans */
 
-#define HZ		60		/* clock freq (software settable on IBM-PC) */
+#define HZ		60	        	/* clock freq (software settable on IBM-PC) */
 #define BLOCK_SIZE	1024		/* # bytes in a disk block */
 #define SUPER_USER	(uid) 0		/* uid of superuser */
 
-#define MAJOR		8		/* major device = (dev>>MAJOR) & 0377 */
-#define MINOR		0		/* minor device = (dev>>MINOR) & 0377 */
+#define MAJOR		8	    	/* major device = (dev>>MAJOR) & 0377 */
+#define MINOR		0	    	/* minor device = (dev>>MINOR) & 0377 */
