9f9fa80a858c7a3f030f0e103c88ed18157adca5
elma <elma@fake.email.com>
2026-08-26 17:25:19 -0500

    Continue working on const.h in directory h

--- MyNIX_1.0/MyNIX_1.0_C89_i8088/h/const.h
+++ MyNIX_1.0/MyNIX_1.0_C89_i8088/h/const.h
@@ -8,7 +8,7 @@
  */
 
 /*
- * Directory h contains files defining constants, types, and macros needed  in
+ * Directory h contains files defining constants, types, and macros needed in
  * more than one of the following independent parts (programs) of MINIX:
  *  1. Kernel
  *  2. Memory Manager
@@ -20,6 +20,17 @@
  * Sections 2.6.1 - 2.6.2
  */
 
-#define EXTERN		extern	/* used in *.h files */
-#define PRIVATE		static	/* PRIVATE x limits the scope of x */
+#define EXTERN		extern		/* used in *.h files */
+#define PRIVATE		static		/* PRIVATE x limits the scope of x */
+#define PUBLIC				/* PUBLIC is the opposite of PRIVATE */
+#define FORWARD				/* some compilers require this to be 'static' */
 
+#define TRUE		1		/* used for turning integers into Booleans */
+#define FALSE		0		/* used for turning integers into Booleans */
+
+#define HZ		60		/* clock freq (software settable on IBM-PC) */
+#define BLOCK_SIZE	1024		/* # bytes in a disk block */
+#define SUPER_USER	(uid) 0		/* uid of superuser */
+
+#define MAJOR		8		/* major device = (dev>>MAJOR) & 0377 */
+#define MINOR		0		/* minor device = (dev>>MINOR) & 0377 */
