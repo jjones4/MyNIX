15074496b18e7b9387e6bbabf4205b0d782c8a66
elma <elma@fake.email.com>
2026-08-26 16:52:22 -0500

    Add file const.h in h directory

--- MyNIX_1.0/MyNIX_1.0_C89_i8088/h/const.h
+++ MyNIX_1.0/MyNIX_1.0_C89_i8088/h/const.h
@@ -1,1 +1,14 @@
-/* #define EXTERN	extern
+/*
+ * Directory h contains files defining constants, types, and macros needed  in
+ * more than one of the following independent parts (programs) of MINIX:
+ *  1. Kernel
+ *  2. Memory Manager
+ *  3. File System
+ *  4. Init
+ *
+ * (Operating Systems Design and Implementation, Tanenbaum, pp. 91-92.)
+ */
+
+#define EXTERN		extern	/* used in *.h files */
+#define PRIVATE		static	/* PRIVATE x limits the scope of x */
+
