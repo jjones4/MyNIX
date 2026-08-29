ff97d89b1b846fd5cad3d3b1789a77ffe8dfe315
jjones <26050634+jjones4@users.noreply.github.com>
2026-08-29 06:46:44 -0500

    Update const.h

--- i8088/h/const.h
+++ i8088/h/const.h
@@ -10,14 +10,16 @@
 /*
  * Directory h contains files defining constants, types, and macros needed in
  * more than one of the following independent parts (programs) of MINIX:
- *  1. Kernel
- *  2. Memory Manager
- *  3. File System
- *  4. Init
+ *  1. bootblok
+ *  2. Kernel
+ *  3. Memory Manager
+ *  4. File System
+ *  5. Init
+ *  6. fsck
  *
  * Tanenbaum, Andrew S. 1987. Operating Systems: Design and Implementation.
  * Englewood Cliffs, NJ: Prentice-Hall.
- * Sections 2.6.1 - 2.6.2
+ * Sections 2.6.1 - 2.6.2 and Appendix D.5.0
  */
 
 #define EXTERN        extern	/* used in *.h files */
